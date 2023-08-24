f5525277ea472ae170d457f2cc8f43c6cba70c72
Naiara Santos <127265827+NaiaraSantos3@users.noreply.github.com>
2023-08-23 22:22:30 -0300

    Lista 1

---
+++ LP/Exercicio2a.java
@@ -0,0 +1,9 @@
+public class Exercicio2a {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        System.out.println(b);
+        b = 5;
+        System.out.println(a + ";" + b);
+    }
+}
---
+++ LP/Exercicio2c.java
@@ -0,0 +1,11 @@
+public class Exercicio2c {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 0;
+        c = a;
+        b = c;
+        a = b;
+        System.out.println(a + ";" + b + ";" + c);
+    }
+}
