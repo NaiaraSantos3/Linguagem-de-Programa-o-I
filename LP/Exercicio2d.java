dc1917daaa9d43a51d44a87b04b1df2bb84069aa
Naiara Santos <127265827+NaiaraSantos3@users.noreply.github.com>
2023-08-31 19:49:38 -0300

    Delete LP directory

--- LP/Exercicio2d.java
+++
@@ -1,9 +0,0 @@
-public class Exercicio2d {
-    public static void main(String[] args) {
-        int a = 10;
-        int b = 20;
-        a = b + 1;
-        b = a + 1;
-        System.out.println(a + ";" + b);
-    }
-}
