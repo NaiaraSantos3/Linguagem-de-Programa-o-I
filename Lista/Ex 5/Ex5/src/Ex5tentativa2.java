1eb6141a5f697b492235e40205d4dbc67d09a077
Naiara Santos <127265827+NaiaraSantos3@users.noreply.github.com>
2023-08-31 21:16:06 -0300

    Add files via upload

---
+++ Lista/Ex 5/Ex5/src/Ex5tentativa2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner; // importa a Classe Scanner
+
+public class Ex5tentativa2 {
+    public static void main(String[] args){
+    Scanner ler = new Scanner (System.in); //Iniciar e criar um objeto Scanner
+    int a = ler.nextInt;
+    System.out.println("Insira um valor:");
+    antecessor = a - 1;
+
+
+
+    }
+}
